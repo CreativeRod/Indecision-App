e1fd4f6198b3bd55097d3f8faa55dc3ec0f8042c
Creative Roderick <CreativeRoderick@gmail.com>
2017-10-13 08:39:52 -0500

    Installed and configured SASS with webpack, and started styling application

--- src/components/Option.js
+++ src/components/Option.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 // This is a stateless functional component => does not manage state and does not have access to 'this'
 const Option = (props) => (
-    <div>
-      {props.optionText}
+    <div className="option">
+      <p className="option__text">{props.count}. {props.optionText}</p>      
       <button
+        className="button button--link"
         onClick={(e) => {
           props.handleDeleteOption(props.optionText);
         }}
--- src/components/Options.js
+++ src/components/Options.js
@@ -4,17 +4,25 @@ import Option from './Option';
 // This is a stateless functional component => does not manage state and does not have access to 'this'
 const Options = (props) => (
     <div>
-      <button onClick={props.handleDeleteOptions}>Remove All</button>
-    {props.options.length === 0 && <p>Please Add An Option To Get Started</p>}
-      {
-        props.options.map((option) => (
-          <Option
-            key={option}
-            optionText={option}
-            handleDeleteOption={props.handleDeleteOption}
-          />
-        ))
-      }
+      <div className="widget-header">
+        <h3 className="widget-header__title">Your Options</h3>
+        <button
+          className="button button--link"
+          onClick={props.handleDeleteOptions}>
+          Remove All
+        </button>
+      </div>
+      {props.options.length === 0 && <p className="widget__message">Please Add An Option To Get Started</p>}
+        {
+          props.options.map((option, index) => (
+            <Option
+              key={option}
+              optionText={option}
+              count={index + 1}
+              handleDeleteOption={props.handleDeleteOption}
+            />
+          ))
+        }
     </div>
   );
 
